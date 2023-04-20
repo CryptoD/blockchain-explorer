d71767c522a3b862998e59095435fc7733f689f0
CryptoD <ilw2vp7ymxa9@blurmail.net>
2023-04-20 22:29:43 +0400

    Updates to code. After those I no longer get any errors after "go build"

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
@@ -35,9 +36,9 @@ require (
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"database/sql"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"strconv"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.Header("Content-Type", "text/html")
+		c.File("index.html")
+	})
+
+	router.Run(":8080")
 
 	// Connect to Ethereum node
 	client, err := ethclient.Dial("https://delicate-twilight-silence.discover.quiknode.pro/d07e2d37b61844a0677b85210c8074b063e3b3d8/")
@@ -45,30 +54,115 @@ func main() {
 	router.Run(":3000") // Listen on port 3000
 }
 
-func main2() {
-	// Read environment variables
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-
-	// Connect to the PostgreSQL database server
-	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
-	db, err := sql.Open("postgres", connectionString)
-	if err != nil {
-		log.Fatal(err)
+func searchBlockchain(query string) (string, map[string]interface{}, error) {
+	// Check if the query is an address, transaction ID, or block height
+	if isValidAddress(query) {
+		data, err := getAddressDetails(query)
+		return "address", data, err
+	} else if isValidTransactionID(query) {
+		data, err := getTransactionDetails(query)
+		return "transaction", data, err
+	} else if isValidBlockHeight(query) {
+		data, err := getBlockDetails(query)
+		return "block", data, err
+	} else {
+		return "", nil, errors.New("invalid search query")
 	}
-	defer db.Close()
+}
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("q")
+	if query == "" {
+		http.Error(w, "query parameter 'q' is required", http.StatusBadRequest)
+		return
 	}
 
-	router := gin.Default()
+	resultType, data, err := searchBlockchain(query)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
 
-	// Your routes and handlers go here
+	response := map[string]interface{}{
+		"resultType": resultType,
+		"data":       data,
+	}
 
-	router.Run(":3000") // Listen on port 3000
+	json.NewEncoder(w).Encode(response)
+}
+
+const blockchairBaseURL = "https://api.blockchair.com/bitcoin"
+
+func isValidAddress(address string) bool {
+	// Bitcoin addresses are usually 26-35 characters long.
+	if len(address) >= 26 && len(address) <= 35 {
+		return true
+	}
+	return false
+}
+
+func getAddressDetails(address string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("%s/dashboards/address/%s", blockchairBaseURL, address)
+	response, err := blockchairRequest(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	json.Unmarshal(response.Body(), &result)
+
+	return result, nil
+}
+
+func isValidTransactionID(txID string) bool {
+	// Bitcoin transaction IDs are 64 characters long hexadecimal strings.
+	if len(txID) == 64 {
+		return true
+	}
+	return false
+}
+
+func getTransactionDetails(txID string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("%s/dashboards/transaction/%s", blockchairBaseURL, txID)
+	response, err := blockchairRequest(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	json.Unmarshal(response.Body(), &result)
+
+	return result, nil
+}
+
+func isValidBlockHeight(blockHeight string) bool {
+	// A simple check to see if the blockHeight string can be converted to an integer
+	_, err := strconv.Atoi(blockHeight)
+	return err == nil
+}
+
+func getBlockDetails(blockHeight string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("%s/dashboards/block/%s", blockchairBaseURL, blockHeight)
+	response, err := blockchairRequest(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	json.Unmarshal(response.Body(), &result)
+
+	return result, nil
+}
+
+func blockchairRequest(url string) (*resty.Response, error) {
+	client := resty.New()
+
+	response, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		Get(url)
+
+	return response, err
 }
